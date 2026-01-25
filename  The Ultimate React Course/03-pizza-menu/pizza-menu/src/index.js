420a872544342733a3c0045277dfa19a85bf3525
gregorychernyavskiy <gregory.chernyavskiy@gmail.com>
2026-01-25 12:44:56 -0600

    .

---  The Ultimate React Course/03-pizza-menu/pizza-menu/src/index.js
+++  The Ultimate React Course/03-pizza-menu/pizza-menu/src/index.js
@@ -98,15 +98,19 @@ function Menu() {
 
 
 function Pizza(props) {
-  if(props.pizzaObj.soldOut) return null;
+//   if(props.pizzaObj.soldOut) return null;
+
 
   return (
-    <li className="pizza">
+    <li className={`pizza ${props.pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price + 3}</span>
+
+        {/* {props.pizzaObj.soldOut ? <span>SOLD OUT</span> : <span>props.pizzaObj.price</span>} */}
+
+        <span>{props.pizzaObj.soldOut ? "SOLD OUT" : props.pizzaObj.price}</span>
       </div>
     </li>
   );
